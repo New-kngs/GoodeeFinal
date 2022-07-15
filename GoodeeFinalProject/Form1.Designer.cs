b1c08858fe8ae7f31f92310b57bb97d2512f2415
min <min@DESKTOP-JJMJOMO>
2022-07-15 16:53:51 +0900

    TEST

--- GoodeeFinalProject/Form1.Designer.cs
+++ GoodeeFinalProject/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace GoodeeFinalProject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 401);
+            this.ClientSize = new System.Drawing.Size(863, 401);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
