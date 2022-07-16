9483f868a6e2c1b743e22ad68f70830dd826da92
min <min@DESKTOP-JJMJOMO>
2022-07-16 12:37:19 +0900

    이미지테스트

---
+++ AltasMES/Main.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace AltasMES
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.MaintoolStrip = new System.Windows.Forms.ToolStrip();
+            this.StandardStrip = new System.Windows.Forms.ToolStripButton();
+            this.SalesStrip = new System.Windows.Forms.ToolStripButton();
+            this.ProductionStrip = new System.Windows.Forms.ToolStripButton();
+            this.LogOutStrip = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            this.MaintoolStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(136, 417);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(136, 80);
+            this.panel2.TabIndex = 3;
+            // 
+            // MaintoolStrip
+            // 
+            this.MaintoolStrip.BackColor = System.Drawing.Color.White;
+            this.MaintoolStrip.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.MaintoolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StandardStrip,
+            this.SalesStrip,
+            this.ProductionStrip,
+            this.LogOutStrip});
+            this.MaintoolStrip.Location = new System.Drawing.Point(136, 0);
+            this.MaintoolStrip.Name = "MaintoolStrip";
+            this.MaintoolStrip.Size = new System.Drawing.Size(776, 72);
+            this.MaintoolStrip.TabIndex = 2;
+            this.MaintoolStrip.Text = "toolStrip1";
+            // 
+            // StandardStrip
+            // 
+            this.StandardStrip.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.StandardStrip.Image = ((System.Drawing.Image)(resources.GetObject("StandardStrip.Image")));
+            this.StandardStrip.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.StandardStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.StandardStrip.Margin = new System.Windows.Forms.Padding(10, 1, 10, 2);
+            this.StandardStrip.Name = "StandardStrip";
+            this.StandardStrip.Size = new System.Drawing.Size(83, 69);
+            this.StandardStrip.Text = "기준정보관리";
+            this.StandardStrip.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.StandardStrip.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // SalesStrip
+            // 
+            this.SalesStrip.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.SalesStrip.Image = ((System.Drawing.Image)(resources.GetObject("SalesStrip.Image")));
+            this.SalesStrip.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.SalesStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SalesStrip.Margin = new System.Windows.Forms.Padding(10, 1, 10, 2);
+            this.SalesStrip.Name = "SalesStrip";
+            this.SalesStrip.Size = new System.Drawing.Size(59, 75);
+            this.SalesStrip.Text = "영업관리";
+            this.SalesStrip.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // ProductionStrip
+            // 
+            this.ProductionStrip.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ProductionStrip.Image = ((System.Drawing.Image)(resources.GetObject("ProductionStrip.Image")));
+            this.ProductionStrip.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.ProductionStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ProductionStrip.Margin = new System.Windows.Forms.Padding(10, 1, 10, 2);
+            this.ProductionStrip.Name = "ProductionStrip";
+            this.ProductionStrip.Size = new System.Drawing.Size(59, 75);
+            this.ProductionStrip.Text = "생산관리";
+            this.ProductionStrip.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // LogOutStrip
+            // 
+            this.LogOutStrip.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.LogOutStrip.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LogOutStrip.Image = ((System.Drawing.Image)(resources.GetObject("LogOutStrip.Image")));
+            this.LogOutStrip.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.LogOutStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LogOutStrip.Margin = new System.Windows.Forms.Padding(0, 5, 0, 2);
+            this.LogOutStrip.Name = "LogOutStrip";
+            this.LogOutStrip.Size = new System.Drawing.Size(59, 65);
+            this.LogOutStrip.Text = "로그아웃";
+            this.LogOutStrip.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(136, 72);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 26);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(136, 395);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(776, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(121, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 417);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.MaintoolStrip);
+            this.Controls.Add(this.panel1);
+            this.IsMdiContainer = true;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.MaintoolStrip.ResumeLayout(false);
+            this.MaintoolStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolStrip MaintoolStrip;
+        private System.Windows.Forms.ToolStripButton StandardStrip;
+        private System.Windows.Forms.ToolStripButton SalesStrip;
+        private System.Windows.Forms.ToolStripButton ProductionStrip;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripButton LogOutStrip;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
